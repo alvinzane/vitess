534451aee90fdc08b104970254d3b8d34eaf8d9b
Sugu Sougoumarane <ssougou@gmail.com>
2019-01-29 10:32:43 -0800

    vreplication: demo on new code
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

---
+++ examples/vdemo/vproduct.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Copyright 2018 The Vitess Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
+
+import subprocess
+
+import MySQLdb as db
+
+import positions
+
+cmd = [
+  './lvtctl.sh',
+  'VReplicationExec',
+  'test-200',
+  """insert into _vt.vreplication
+  (source, pos, max_tps, max_replication_lag, tablet_types, time_updated, transaction_timestamp, state) values
+  ('keyspace:"lookup" shard:"0" filter:<rules:<match:"uproduct" filter:"select * from product" > >', 'MySQL56/%s', 9999, 9999, 'master', 0, 0, 'Running')""" % positions.positions[100],
+  ]
+
+print "executing:", cmd
+subprocess.call(cmd)
+
+cmd = [
+  './lvtctl.sh',
+  'VReplicationExec',
+  'test-300',
+  """insert into _vt.vreplication
+  (source, pos, max_tps, max_replication_lag, tablet_types, time_updated, transaction_timestamp, state) values
+  ('keyspace:"lookup" shard:"0" filter:<rules:<match:"uproduct" filter:"select * from product" > >', 'MySQL56/%s', 9999, 9999, 'master', 0, 0, 'Running')""" % positions.positions[100],
+  ]
+
+print "executing:", cmd
+subprocess.call(cmd)
